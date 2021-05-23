0d32cfec82c6e2e2fb840a40f02df5051faeb2d3
阿飞 <43526141+ltfafei@users.noreply.github.com>
2021-05-23 10:44:31 +0800

    Update scan.bat

--- scan.bat
+++ scan.bat
@@ -1,8 +1,8 @@
 @echo off
 echo --------------------------------------
-echo AWVS调用API批量扫描脚本
+echo AWVS璋冪敤API鎵归噺鎵弿鑴氭湰
 echo --------------------------------------
-echo 正在添加任务...
+echo 姝ｅ湪娣诲姞浠诲姟...
 
-py -3 awvs_console.py -a localhost -k 1986ad8c0a5b3df4d7028d5f3c06e936c7ba11cf78a694334b01a5aa511dfca91  -f "D:\penetration testing\All-sec\20210506-20210514\湖南快乐阳光互动娱乐传媒有限公司\mgtv.com子域名_Layer.txt" -g 芒果TV -s
+py -3 awvs_console.py -a localhost -k 198xxxxxxxxxxxx4b01a5aa511dfca91  -f "Layer.txt" -g xxx -s
 pause
